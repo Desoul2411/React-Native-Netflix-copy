d4e62667032d42f835ce4c28918bca871ce7cc2c
user <user@MacBook-Pro-user.local>
2024-05-08 15:38:04 +0300

    Add Statistics admin page

--- app/components/screens/trending/useTrendidng.ts
+++ app/components/screens/trending/useTrendidng.ts
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 
 import { MovieService } from '@/services/movie.service';
 
-export const useTrending = () => {
+export const useTrending = (limit?: number) => {
 	const { data: movies, isFetching: isLoading } = useQuery({
 		queryKey: ['get trending movies'],
-		queryFn: () => MovieService.getMostPopularMovies()
+		queryFn: () => MovieService.getMostPopularMovies(),
+		select: data => (limit ? data.slice(0, limit) : data)
 	});
 
 	return { movies, isLoading };
